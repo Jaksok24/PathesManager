512c748b84ed88af94c0a564a2fe00c330ec7a95
Jakub Sokołowski <157789383+Jaksok24@users.noreply.github.com>
2025-10-26 10:34:53 +0100

    Update conversation.py

--- services/conversation.py
+++ services/conversation.py
@@ -1,24 +1,5 @@
 import streamlit as st
 
-@st.dialog("Dodaj punkt", dismissible=True)
-def add_point_conversation():
-    nodeId = st.text_input("Node Id", key="cNodeId")
-    isEndNode = st.selectbox("Is End Node", ["true", "false"], key="cIsEndNode")
-    sender = st.selectbox("Sender", ["npc", "user"], key="cSender")
-    message = st.text_area("Message", key="cMessage")
-    
-    if st.button("Dodaj punkt", use_container_width=True, type="primary"):
-        node = {
-            nodeId: {
-                "isEndNode": isEndNode,
-                "sender": sender,
-                "id": nodeId,
-                "message": message
-            }
-        }
-        st.session_state.conversationNodes.append(node)
-        st.rerun()
-
 def add_conversation():
     description = st.text_area("Conversation Description", key="cDescription")
     title = st.text_input("Conversation Title", key="cTitle")
@@ -40,3 +21,37 @@ def add_conversation():
         },
         "nodes": st.session_state.conversationNodes
     }
+
+@st.dialog("Dodaj punkt", width="medium", dismissible=True)
+def add_point_conversation():
+    nodeId = st.text_input("Node Id", key="cNodeId")
+    isEndNode = st.selectbox("Is End Node", ["true", "false"], key="cIsEndNode")
+    sender = st.selectbox("Sender", ["npc", "user"], key="cSender")
+    message = st.text_area("Message", key="cMessage")
+    
+    choices = []
+    with st.form(border=True, clear_on_submit=True, key="choice"):
+        nextNodeId = st.text_input("Next Node Id", key="cNextNodeId")
+        choiceId = st.text_input("Choice Id", key="cChoiceId")
+        choiceText = st.text_input("Choice Text", key="cChoiceText")
+        
+        if st.form_submit_button("Dodaj wybór", use_container_width=True):
+            choice = {
+                "nextNodeId": nextNodeId,
+                "id": choiceId,
+                "text": choiceText
+            }
+            choices.append(choice)
+    
+    if st.button("Dodaj punkt", use_container_width=True, type="primary"):
+        node = {
+            nodeId: {
+                "isEndNode": isEndNode,
+                "sender": sender,
+                "id": nodeId,
+                "message": message,
+                "choices": choices
+            }
+        }
+        st.session_state.conversationNodes.append(node)
+        st.rerun()
